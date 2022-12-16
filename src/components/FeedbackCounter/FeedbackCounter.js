833c61ce4453062c26372272ce72f2dfeb7b67bf
AndreyNik2 <105128068+AndreyNik2@users.noreply.github.com>
2022-12-16 21:07:41 +0200

    Update FeedbackCounter.js

--- src/components/FeedbackCounter/FeedbackCounter.js
+++ src/components/FeedbackCounter/FeedbackCounter.js
@@ -1,71 +1,66 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Statistics from './Statistics';
 import FeedbackOptions from './FeedbackOptions';
 import Notification from './Notification';
 import Section from './Section';
 
-class FeedbackCounter extends React.Component {
-  static defaultProps = {
-    initialBad: 0,
-    initialGood: 0,
-    initialNeutral: 0,
+function FeedbackCounter() {
+  const [bad, setBad] = useState(0);
+  const [good, setGood] = useState(0);
+  const [neutral, setNeutral] = useState(0);
+
+  const handleClickBtn = e => {
+    const { name } = e.target;
+
+    switch (name) {
+      case 'good':
+        setGood(prevState => prevState + 1);
+        break;
+      case 'neutral':
+        setNeutral(prevState => prevState + 1);
+        break;
+      case 'bad':
+        setBad(prevState => prevState + 1);
+        break;
+      default:
+        console.warn('There is no such name');
+    }
   };
 
-  state = {
-    good: this.props.initialGood,
-    neutral: this.props.initialNeutral,
-    bad: this.props.initialBad,
-  };
-
-  handleClickBtn = e => {
-    const { name } = e.currentTarget;
-    this.setState(prevState => ({
-      [name]: prevState[name] + 1,
-    }));
-  };
-
-  countTotalFeedback = () => {
-    const good = this.state.good;
-    const neutral = this.state.neutral;
-    const bad = this.state.bad;
+  const countTotalFeedback = () => {
     return good + neutral + bad;
   };
 
-  countPositiveFeedbackPercentage = () => {
-    const good = this.state.good;
-    const neutral = this.state.neutral;
-    const bad = this.state.bad;
+  const countPositiveFeedbackPercentage = () => {
     if (good === 0) {
       return 0;
     }
     return (good / (good + neutral + bad)) * 100;
   };
 
-  render() {
-    return (
-      <div>
-        <Section title="Please leave feedback">
-          <FeedbackOptions
-            onLeaveFeedback={this.handleClickBtn}
-            options={['good', 'neutral', 'bad']}
+  return (
+    <div>
+      <Section title="Please leave feedback">
+        <FeedbackOptions
+          onLeaveFeedback={handleClickBtn}
+          options={['good', 'neutral', 'bad']}
+        />
+      </Section>
+      <Section title="Statistics">
+        {countTotalFeedback() > 0 ? (
+          <Statistics
+            good={good}
+            neutral={neutral}
+            bad={bad}
+            total={countTotalFeedback()}
+            positivePercentage={countPositiveFeedbackPercentage()}
           />
-        </Section>
-        <Section title="Statistics">
-          {this.countTotalFeedback() > 0 ? (
-            <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
-            />
-          ) : (
-            <Notification message="There is no feedback" />
-          )}
-        </Section>
-      </div>
-    );
-  }
+        ) : (
+          <Notification message="There is no feedback" />
+        )}
+      </Section>
+    </div>
+  );
 }
 
 export default FeedbackCounter;
